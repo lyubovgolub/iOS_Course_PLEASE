a32e0186c58cfe6e8c1120ccf8b142e5496405b8
lyubovgolub <lubovlubov.g@gmail.com>
2020-12-06 20:10:42 +0300

    Homework 2

---
+++ HW_2_types.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+/*:
+ # ЗАДАНИЕ:
+*/
+// В конспекте (файле Playground):
+// 1. Проинициализируйте константы типа Int, Double, String, Bool, не задавая тип явно
+
+let const1 = 4
+let const2 = 4.1
+let const3 = "4"
+let const4 = true
+
+// 2. Проинициализируйте переменные типа Int, Double, String, Bool, явно задав тип
+
+var vaariable1: Int = 4
+var vaariable2: Double = 4.1
+var vaariable3: String = "4"
+var vaariable4: Bool = true
+
+// 3. Проинициализируйте переменную типа Int со значением, равным сумме созданной константе и переменной в п.1-2
+
+var sum1and2 = const1 + vaariable1
+
+// 4. Задайте новое значение переменной, созданной в п.3, равное произведению константы из п.1 и переменной из п.2
+
+sum1and2 = const1 * vaariable1
+
+// 5. Объявите(не инициализируя) переменные типов Int, Double, String
+
+var qwer1: Int
+var qwer2: Double
+var qwer3: String
+
+// 6. Проинициализируйте переменную типа Bool с результатом сравнения константы из п.1 и переменной из п.2
+
+var qwer4: Bool = const1==vaariable1
+
+
+// 7. Проинициализируйте переменую типа String с каким-либо значением
+
+var newstring: String = "word"
+// 8. Взять модуль из переменной типа Int, реализовать округление переменной типа Double
